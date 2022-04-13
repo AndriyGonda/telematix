7c588329c3f90ebaefbdfd82ed91dcc85d3269f9
Andrii Honda <andriy.gonda@gmail.com>
2022-04-13 17:34:30 +0300

    add user table generation

--- domain/src/main/resources/db/migrations/V20220413172512__create_user.sql
+++ domain/src/main/resources/db/migrations/V20220413172512__create_user.sql
@@ -1,11 +1,12 @@
-CREATE TABLE IF NOT EXISTS users(
-  id int GENERATED ALWAYS AS IDENTITY,
-  username VARCHAR(128) UNIQUE,
-  email VARCHAR(128) NOT NULL UNIQUE,
-  password_hash VARCHAR(128),
-  first_name VARCHAR(50),
-  last_name VARCHAR(50),
-  avatar_url varchar(256),
-  administrator BOOLEAN DEFAULT FALSE,
-  PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS users
+(
+    id            int GENERATED ALWAYS AS IDENTITY,
+    username      VARCHAR(128) UNIQUE,
+    email         VARCHAR(128) NOT NULL UNIQUE,
+    password_hash VARCHAR(128),
+    first_name    VARCHAR(50),
+    last_name     VARCHAR(50),
+    avatar_url    varchar(256),
+    administrator BOOLEAN DEFAULT FALSE,
+    PRIMARY KEY (id)
 );
