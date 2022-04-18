90ff2813a5e6031ab16602daddc7f384818c2198
Andrii Honda <andriy.gonda@gmail.com>
2022-04-18 13:29:26 +0300

    add messages table

---
+++ domain/src/main/resources/db/migrations/V20220418125848__create_messages_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS messages
+(
+    id        int GENERATED ALWAYS AS IDENTITY,
+    timestamp TIMESTAMP default now(),
+    raw       VARCHAR(255),
+    sensor_id int,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_sensor FOREIGN KEY (sensor_id) REFERENCES sensors (id)
+);
