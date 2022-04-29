561fd5432d549bbfabf7ecf549f22d3828990848
Andrii Honda <andriy.gonda@gmail.com>
2022-04-29 15:31:56 +0300

    add mqtt support

---
+++ domain/src/main/java/org/telematix/configuration/MqttConfig.java
@@ -0,0 +1,46 @@
+package org.telematix.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageProducer;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+
+@PropertySource("classpath:application.properties")
+public class MqttConfig {
+
+    @Bean
+    public MessageChannel mqttInputChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MessageProducer inbound(
+            @Value("${mqtt.client.rootTopic}") String rootTopic,
+            @Value("${mqtt.client.clientId}") String clientId,
+            @Value("${mqtt.client.url}") String clientUrl,
+            MessageChannel mqttInputChannel
+    ) {
+        MqttPahoMessageDrivenChannelAdapter adapter = new MqttPahoMessageDrivenChannelAdapter(
+                clientUrl, clientId, rootTopic);
+        adapter.setCompletionTimeout(5000);
+        adapter.setConverter(new DefaultPahoMessageConverter());
+        adapter.setQos(1);
+        adapter.setOutputChannel(mqttInputChannel);
+        return adapter;
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "mqttInputChannel")
+    public MessageHandler handler() {
+        return message -> {
+            System.out.println(message.getHeaders().get("mqtt_receivedTopic"));
+            System.out.println(message.getPayload());
+        };
+    }
+}
