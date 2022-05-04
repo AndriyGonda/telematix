cff82905988d577bdbdf2c9c7b7fd9ceab114c05
Andrii Honda <andriy.gonda@gmail.com>
2022-05-04 09:45:47 +0300

    add registration endpoint

--- domain/src/main/java/org/telematix/dto/user/UserCreateDto.java
+++ domain/src/main/java/org/telematix/dto/user/UserCreateDto.java
@@ -35,6 +35,14 @@ public class UserCreateDto {
         return email;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public void setPassword(String password) {
         this.password = password;
     }
--- web/src/main/java/org/telematix/api/user/AuthController.java
+++ web/src/main/java/org/telematix/api/user/AuthController.java
@@ -5,20 +5,29 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.telematix.dto.user.RegisterDto;
 import org.telematix.dto.user.UserCreateDto;
+import org.telematix.dto.user.UserResponseDto;
+import org.telematix.services.UserService;
 
 @RestController
 @RequestMapping("/api")
 public class AuthController {
     private final PasswordEncoder passwordEncoder;
+    private final UserService userService;
 
-    public AuthController(PasswordEncoder passwordEncoder) {
+    public AuthController(PasswordEncoder passwordEncoder, UserService userService) {
         this.passwordEncoder = passwordEncoder;
+        this.userService = userService;
     }
 
     @PostMapping("/register")
-    public UserCreateDto registerUser(@RequestBody UserCreateDto userCreateDto) {
-        userCreateDto.setPassword(passwordEncoder.encode(userCreateDto.getPassword()));
-        return userCreateDto;
+    public UserResponseDto registerUser(@RequestBody RegisterDto registerUserDto) {
+        registerUserDto.setPassword(passwordEncoder.encode(registerUserDto.getPassword()));
+        UserCreateDto userCreateDto = new UserCreateDto();
+        userCreateDto.setPassword(registerUserDto.getPassword());
+        userCreateDto.setUsername(registerUserDto.getUsername());
+        userCreateDto.setEmail(registerUserDto.getEmail());
+        return userService.createUser(userCreateDto);
     }
 }
