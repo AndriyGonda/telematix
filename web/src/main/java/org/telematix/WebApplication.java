9084d07e8848c66ed4ab1353ae15a9011684b3c3
Andrii Honda <andriy.gonda@gmail.com>
2022-05-10 15:32:26 +0300

    add timezone

--- web/src/main/java/org/telematix/WebApplication.java
+++ web/src/main/java/org/telematix/WebApplication.java
@@ -1,13 +1,11 @@
 package org.telematix;
 
-import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebApplication {
     public static void main(String[] args) {
-        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(WebApplication.class, args);
     }
 }
