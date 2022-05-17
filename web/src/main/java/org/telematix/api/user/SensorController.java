381cb7983ffdec8a5f2db79f5855016466e81b46
Andrii Honda <andriy.gonda@gmail.com>
2022-05-17 14:46:54 +0300

    fix annotation

--- web/src/main/java/org/telematix/api/user/SensorController.java
+++ web/src/main/java/org/telematix/api/user/SensorController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.telematix.dto.message.TopicMessageDto;
@@ -23,7 +24,8 @@ import org.telematix.dto.sensor.SensorUpdateDto;
 import org.telematix.services.SensorService;
 
 @Tag(name = "Sensors")
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 public class SensorController {
 
     private final SensorService sensorService;
