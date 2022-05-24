6e5cd7249930c0236b2ac516d9c8baa236576f08
Andrii Honda <andriy.gonda@gmail.com>
2022-05-24 20:42:51 +0300

    tested user detail service

---
+++ domain/src/test/java/org/telematix/services/UserDetailServiceTest.java
@@ -0,0 +1,44 @@
+package org.telematix.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
+
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.telematix.models.User;
+import org.telematix.repositories.UserRepository;
+
+@ExtendWith(MockitoExtension.class)
+class UserDetailServiceTest {
+    @Mock
+    UserRepository userModelRepository;
+
+    UserDetailService userDetailService;
+
+    @BeforeEach
+    void setUp() {
+        userDetailService = new UserDetailService(userModelRepository);
+    }
+
+    @Test
+    void get_user_details_without_errors() {
+        User user = new User();
+        user.setUsername("test");
+        user.setPasswordHash("test");
+        doReturn(Optional.of(user)).when(userModelRepository).findByUsername("test");
+        UserDetails userDetails = userDetailService.loadUserByUsername("test");
+        assertEquals(userDetails.getUsername(), user.getUsername());
+    }
+
+    @Test
+    void get_user_details_failed_user_empty() {
+        doReturn(Optional.empty()).when(userModelRepository).findByUsername("test");
+        assertThrows(UsernameNotFoundException.class, () -> userDetailService.loadUserByUsername("test"));
+    }
+}
